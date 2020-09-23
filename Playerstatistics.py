d324b3f254d59f2f8e678c1cac055a0e88e5fd9c
Timo Zandbergen <zandbergentimo@gmail.com>
2020-09-23 12:58:53 +0200

    stats

---
+++ Playerstatistics.py
@@ -0,0 +1,28 @@
+name = "name: Timo Zandbergen"
+power = "power: super strong"
+speed = 120.5
+favoriteweapon = "favorite weapon: rocket launcher"
+ammo = "ammo: 12 rockets"
+livesleft = "lives left: 3"
+level = 1
+wantedfor = "wanted for: murder"
+jumpforce = "jumpforce: 15"
+movement = "movement: bike"
+
+
+level+=9
+
+speed *=20
+
+
+
+print(name)
+print(power)
+print("speed: "+ str (speed))
+print(favoriteweapon)
+print(ammo)
+print(livesleft)
+print("level: "+ str (level))
+print(wantedfor) 
+print(jumpforce)
+print(movement)
