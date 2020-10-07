38eb3530b96fd1c217913b40177420570367fd69
Timo Zandbergen <zandbergentimo@gmail.com>
2020-10-07 12:16:11 +0200

    operations

--- myfirstoperations.py
+++ myfirstoperations.py
@@ -2,13 +2,21 @@ import time
 name = "Timo" 
 print(name + " " + "Zandbergen")
 
-leeftijd = 21
+leeftijd = 19 + 2 
 print(leeftijd - 5)
 
 x = 15
 y = 4
 
 print(x / y)
+
+klas = 5 * 5 
+klas-=24
+print(klas)
+
+modulo = 45 % 12
+modulo%=35
+print(modulo) 
 
 f = 7
 
@@ -23,5 +31,7 @@ print(nummer)
 getal = 43
 getal *= 4
 print(getal)
+
+
 
 
