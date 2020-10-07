8361d29056a5e918785e45360aff3fb3ba5daf98
Timo Zandbergen <zandbergentimo@gmail.com>
2020-10-07 12:01:02 +0200

    operations

---
+++ myfirstoperations.py
@@ -0,0 +1,27 @@
+import time
+name = "Timo" 
+print(name + " " + "Zandbergen")
+
+leeftijd = 21
+print(leeftijd - 5)
+
+x = 15
+y = 4
+
+print(x / y)
+
+f = 7
+
+f%=3
+
+print(f)
+
+nummer = 18
+nummer/= 6
+print(nummer)
+
+getal = 43
+getal *= 4
+print(getal)
+
+
